049e32f23275ebe086a8e148d2985a366a3f127b
Donnie Wang <donnie.wang@striking.ly>
2018-12-29 01:08:25 +0800

    add readme

---
+++ index.scm
@@ -0,0 +1,6 @@
+(define (square x) (* x x))
+(define (pytha x y) (sqrt (+ (square x)
+															(square y))))
+
+
+(pytha 6 8)
---
+++ readme.md
@@ -0,0 +1,1 @@
+this repository is for studying `Structure and Interpretation of Computer Programs`
