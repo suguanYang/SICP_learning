2e255277dcde242c677ca25bc35fce4c19b1b919
Donnie Wang <donnie.wang@striking.ly>
2019-03-14 01:42:21 +0800

    add gcd

---
+++ chaper_one/gcd.scm
@@ -0,0 +1,8 @@
+;;; 欧几里德算法 if r is a remainder of a,b. the common divisor of a,b also is the common divisor of b,r
+;;; so we can simplfy GCD(a, b) = GCD(b,r)
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (let ((r (remainder a b)))
+        (gcd b r))))
